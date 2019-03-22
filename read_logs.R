807ec91ed7a82124e93143789db50a6f167db745
Phillip Doehle <doehle@okstate.edu>
2019-03-22 10:48:15 -0500

    Create tibble with filenames and timestamps
    Information about log files are kept in named lists that can
    be added to as more types of log files are brought into the system.
    Code creates a tibble containing each filename, the log type, and
    the log date that is parsed from the filename using information
    contained in the named lists.

--- read_logs.R
+++ read_logs.R
@@ -1,6 +1,23 @@
 library("tidyverse")
 library("RSQLite")
 
-# Get the list of log files for each type of log
-filenames <- list(scratch_logs = "[[:digit:]]*scratchuse", 
-                  project_logs = "[[:digit:]]*project[[:digit:]]*quotalog.txt")
+# Lists of log information
+log_types <- c("scratch", "project")
+# Patterns for getting list of files
+file_patterns <- list(scratch = "[[:digit:]]*scratchuse", 
+                  project = "[[:digit:]]*project[[:digit:]][[:digit:]]quotalog.txt")
+# Patterns for extracting dates from filenames
+file_date <- list(scratch = "scratchuse",
+                  project = "project[[:digit:]][[:digit:]]quotalog.txt")
+
+# Get list of log filenames, types, and dates for each type of log
+log_files <- tibble(filename = character(), log_type = character(), day = date())
+for(type in log_types){
+  log_files <- log_files %>% add_row(filename = dir(path = "./logs/", 
+                                                    pattern = as.character(file_patterns[type])),
+                                     log_type = type,
+                                     day = gsub(file_date[type], "", filename) %>% 
+                                       as.Date(format = "%Y%m%d") %>% 
+                                       format("%Y-%m-%d"))
+}
+
